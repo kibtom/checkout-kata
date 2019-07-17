60e47540cc7af562787c78fde407fed63f2f36ea
Thomas Kibaru <kibtom@yahoo.com>
2019-07-17 21:41:47 +0100

    Add interface folder. ICheckout added

---
+++ CheckoutKata/KataInterface/ICheckout.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata.KataInterface
+{
+    interface ICheckout
+    {
+        void scan(string product);
+        decimal GetTotalPrice();
+    }
+}
