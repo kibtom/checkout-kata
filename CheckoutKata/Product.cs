18f1d664877e7aba0baa69130fb781799e2876fb
Thomas Kibaru <kibtom@yahoo.com>
2019-07-17 15:28:40 +0100

    Implement Product class and Add Class for catalog.

---
+++ CheckoutKata/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata
+{
+    public class Product
+    {
+        public string Sku;
+        public int Price;
+
+        public Product(string sku, int price)
+        {
+            Price = price;
+            Sku = sku;
+        }
+    }
+}
